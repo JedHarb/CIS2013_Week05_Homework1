86e7b19ce539d2de29e3a3b30d9bd56a97693bbc
JedHarb <jedharb@gmail.com>
2018-02-14 20:33:27 -0600

    balnk file setup for homework week 5
    balnk file setup for homework week 5

---
+++ CIS2013_Week05_Homework1/Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// homework is to ask for a month, and a year. then print up a calendar for that month with the correct days of the week ( s m t w t f s )
+	// find the day of the week that the year started on, then you can run a calculator (function) to find the day each month starts on
+	return 0;
+}
